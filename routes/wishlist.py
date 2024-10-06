56a182d342bf0717d21ddb20fac204fdcd1a316a
ckhei <wongchekhei.11810@gmail.com>
2024-10-06 16:50:59 +0800

    updated search, not complete

--- routes/wishlist.py
+++ routes/wishlist.py
@@ -30,10 +30,20 @@ def view_wishlist():
         print(session['user_id'])
 
         get_table_query = """
-            SELECT username, title, added_date, wanted_game.user_id, wanted_game.game_id FROM wanted_game
-            INNER JOIN user ON wanted_game.user_id = user.user_id
-            INNER JOIN game ON wanted_game.game_id = game.game_id
-            WHERE user.user_id = %s;
+            SELECT 
+                g.title, 
+                wg.added_date, 
+                wg.game_id,
+                GROUP_CONCAT(DISTINCT d.developer_name SEPARATOR ', ') AS developer_name,
+                GROUP_CONCAT(DISTINCT c.category_name SEPARATOR ', ') AS categories 
+            FROM wanted_game wg
+            INNER JOIN game g ON wg.game_id = g.game_id
+            INNER JOIN game_developer gd ON wg.game_id = gd.game_id
+            INNER JOIN developer d ON gd.developer_id = d.developer_id
+            INNER JOIN game_category gc ON wg.game_id = gc.game_id
+            INNER JOIN category c ON gc.category_id = c.category_id
+            WHERE wg.user_id = %s
+            GROUP BY g.title, wg.game_id;
         """
         
         cur.execute(
@@ -42,6 +52,8 @@ def view_wishlist():
             )
         
         wishlist = cur.fetchall()
+        
+        print(wishlist)
 
     except mysql.connector.Error as e:
         print(f"Error: {e}")
@@ -50,7 +62,7 @@ def view_wishlist():
         cur.close()
         conn.close()
     
-    return render_template("wishlist/wishlist.html", wishlist=wishlist)
+    return render_template("wishlist/wishlist.html", wishlist=wishlist, search = False)
 
 @wishlist_bp.route("/add-to-wishlist", methods=["GET", "POST"])
 def addToWishlist():
@@ -96,16 +108,15 @@ def addToWishlist():
     return view_wishlist()
 
 
-@wishlist_bp.route("/delete-from-wishlist/<user_id>/<game_id>", methods=["GET"])
-def deleteFromWishlist(user_id, game_id):
+@wishlist_bp.route("/delete-from-wishlist/<game_id>", methods=["GET"])
+def deleteFromWishlist(game_id):
     conn = create_connection()
     if conn is None:
         return "Failed to connect to database"
     try:
         cur = conn.cursor(dictionary=True)
         
-        print("this is the user:", user_id)
-        print("this is the game:", game_id)
+        user_id = session['user_id']
 
         delete_table_query = """
             DELETE FROM wanted_game
@@ -129,73 +140,107 @@ def deleteFromWishlist(user_id, game_id):
 
     return view_wishlist()
 
-@wishlist_bp.route("/search-wishlist")
+@wishlist_bp.route("/search-wishlist", methods=["GET", "POST"])
 def searchWishlist():
-    conn = create_connection()
-    if conn is None:
-        return "Failed to connect to database"
-    try:
-        cur = conn.cursor(dictionary=True)
-        
-        string = "City Medal"
+    if request.method == "POST":
+        conn = create_connection()
+        if conn is None:
+            return "Failed to connect to database"
+        try:
+            cur = conn.cursor(dictionary=True)
+            
+            string = request.form["searchInput"]
+            
+            search_table_query1 = """
+                SELECT 
+                    g.title, 
+                    GROUP_CONCAT(DISTINCT d.developer_name SEPARATOR ', ') AS developer_name,
+                    GROUP_CONCAT(DISTINCT c.category_name SEPARATOR ', ') AS categories, 
+                    wg.game_id,
+                    wg.added_date
+                FROM 
+                    wanted_game wg
+                INNER JOIN 
+                    game g ON wg.game_id = g.game_id
+                INNER JOIN 
+                    game_developer gd ON wg.game_id = gd.game_id
+                INNER JOIN 
+                    developer d ON gd.developer_id = d.developer_id
+                INNER JOIN 
+                    game_category gc ON wg.game_id = gc.game_id
+                INNER JOIN 
+                    category c ON gc.category_id = c.category_id
+                WHERE 
+                    wg.user_id = '{}'
+                AND 
+                    (
+                        (g.title LIKE "%{}%" OR d.developer_name LIKE "%{}%" OR c.category_name LIKE "%{}%")
+                    )
+                GROUP BY 
+                    g.title, wg.game_id;
+            """.format(session['user_id'], string, string, string)
+            
+            # Execute the SQL statement
+            cur.execute(search_table_query1)
+            
+            searchResult = cur.fetchall()
+            print("RESULT 1 here:")
+            print(searchResult)
+            
+            if (searchResult == []):
+                search_table_query2 = """
+                    SELECT 
+                        g.title, 
+                        GROUP_CONCAT(DISTINCT d.developer_name SEPARATOR ', ') AS developer_name,
+                        GROUP_CONCAT(DISTINCT c.category_name SEPARATOR ', ') AS categories, 
+                        wg.game_id,
+                        wg.added_date
+                    FROM 
+                        wanted_game wg
+                    INNER JOIN 
+                        game g ON wg.game_id = g.game_id
+                    INNER JOIN 
+                        game_developer gd ON wg.game_id = gd.game_id
+                    INNER JOIN 
+                        developer d ON gd.developer_id = d.developer_id
+                    INNER JOIN 
+                        game_category gc ON wg.game_id = gc.game_id
+                    INNER JOIN 
+                        category c ON gc.category_id = c.category_id
+                    WHERE 
+                        wg.user_id = '{}'
+                    AND 
+                        (
+                """.format(session['user_id'])
+                
+                count = len(string.split())
+                for i in string.split():
+                    search_table_query2 += """
+                        (g.title LIKE "%{}%" OR d.developer_name LIKE "%{}%" OR c.category_name LIKE "%{}%")
+                        """.format(i, i, i)
+                    count -= 1
+                    if count != 0:
+                        search_table_query2 += "OR"
+                
+                search_table_query2 += """
+                        )
+                    GROUP BY 
+                        g.title, wg.game_id;
+                    """
 
-        search_table_query = """
-            SELECT 
-                g.title, 
-                g.price, 
-                d.developer_name, 
-                GROUP_CONCAT(DISTINCT c.category_name SEPARATOR ', ') AS categories, 
-                wg.game_id
-            FROM 
-                wanted_game wg
-            INNER JOIN 
-                game g ON wg.game_id = g.game_id
-            INNER JOIN 
-                game_developer gd ON wg.game_id = gd.game_id
-            INNER JOIN 
-                developer d ON gd.developer_id = d.developer_id
-            INNER JOIN 
-                game_category gc ON wg.game_id = gc.game_id
-            INNER JOIN 
-                category c ON gc.category_id = c.category_id
-            WHERE 
-                wg.user_id = '{}'
-            AND 
-                (
-        """.format(session['user_id'])
-        
-        count = len(string.split())
-        for i in string.split():
-            search_table_query += """
-                (g.title LIKE '%{}%' OR d.developer_name LIKE '%{}%' OR c.category_name LIKE '%{}%')
-                """.format(i, i, i)
-            count -= 1
-            if count != 0:
-                search_table_query += "OR"
-        
-        search_table_query += """
-                )
-            GROUP BY 
-                g.title, g.price, d.developer_name, wg.game_id
-            ORDER BY 
-                g.title ASC;
-            """
-        
-        print(search_table_query)
+                # Execute the SQL statement
+                cur.execute(search_table_query2)
+                
+                searchResult = cur.fetchall()
+                print("RESULT:")
+                print(searchResult)   
 
-        # Execute the SQL statement
-        cur.execute(search_table_query)
-        
-        searchResult = cur.fetchall()
-        print("RESULT:")
-        print(searchResult)
+        except mysql.connector.Error as e:
+            conn.rollback()
+            print(f"Error: {e}")
+            return f"Error searching from wanted_games: {e}"
+        finally:
+            cur.close()
+            conn.close()
 
-    except mysql.connector.Error as e:
-        conn.rollback()
-        print(f"Error: {e}")
-        return f"Error searching from wanted_games: {e}"
-    finally:
-        cur.close()
-        conn.close()
-
-    return render_template("wishlist/wishlist.html", searchResult=searchResult)
+    return render_template("wishlist/wishlist.html", search=True, searchResult=searchResult, string=string)
